961eb69b9de8a078c155847ecebf0be6e0838d16
Makoto <makoto@makoto.io>
2016-03-13 22:05:17 -0400

    Execute setup on first run

--- tumdlr/main.py
+++ tumdlr/main.py
@@ -2,6 +2,8 @@ import logging
 import os
 import pkgutil
 
+from subprocess import call
+
 import click
 
 from tumdlr.config import load_config
@@ -58,27 +60,23 @@ class CommandLine(click.MultiCommand):
 pass_context = click.make_pass_decorator(Context, ensure=True)
 
 
+# noinspection PyIncorrectDocstring
 @click.command(cls=CommandLine, context_settings=CONTEXT_SETTINGS)
 @click.option('-c', '--config', type=click.Path(dir_okay=False, resolve_path=True), envvar='TUMDLR_CONFIG_PATH',
-              default='~/.config/tumdlr/tumdlr.conf', help='Path to the TumDLR configuration file')
+              default='~/.config/tumdlr/tumdlr.conf',
+              help='Path to the TumDLR configuration file (currently does nothing)')
 @click.option('-q', '--quiet', help='Silence all output except for fatal errors', is_flag=True)
 @click.option('-d', '--debug', help='Output information used for debugging', is_flag=True)
 @pass_context
 def cli(ctx, config, quiet, debug):
     """
     Tumblr Downloader CLI utility
-
-    Args:
-        ctx(Context)
-        config(str)
-        quiet(bool)
-        debug(bool)
     """
     # Logging setup
     if debug:
         log_level = logging.DEBUG
     else:
-        log_level = logging.ERROR if quiet else logging.WARN
+        log_level = logging.CRITICAL if quiet else logging.WARN
 
     ctx.log = logging.getLogger('tumdlr')
     ctx.log.setLevel(log_level)
@@ -87,6 +85,29 @@ def cli(ctx, config, quiet, debug):
     ch.setLevel(log_level)
     ch.setFormatter(logging.Formatter('[%(levelname)s] %(name)s: %(message)s'))
     ctx.log.addHandler(ch)
+
+    # First run?
+    if not ctx.config['Development'].getboolean('AgreedToTerms'):
+        first_run(ctx)
+
+
+def first_run(ctx):
+    """
+    Run the setup and other tasks for first-time use
+
+    Args:
+        ctx(Context)
+    """
+    terms_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'TERMS.rst')
+
+    # Display the welcome message / terms and conditions agreement
+    if os.name == 'nt':
+        call(['more', terms_path])
+    else:
+        call(['less', terms_path])
+
+    # Run the setup command
+    ctx.invoke('setup')
 
 
 if __name__ == '__main__':
